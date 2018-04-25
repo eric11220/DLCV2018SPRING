61916c3450dc0cd826baed9f6f8458abac806e48
eric11220 <b99902080@ntu.edu.tw>
2018-04-25 20:57:43 +0800

    bilinearUpSampling

--- hw3/src/utils/BilinearUpSampling.py
+++ hw3/src/utils/BilinearUpSampling.py
@@ -1,5 +1,47 @@
 import keras.backend as K
+import tensorflow as tf
 from keras.layers import *
+
+
+def resize_images_bilinear(X, height_factor=1, width_factor=1, target_height=None, target_width=None, data_format='default'):
+    '''Resizes the images contained in a 4D tensor of shape
+    - [batch, channels, height, width] (for 'channels_first' data_format)
+    - [batch, height, width, channels] (for 'channels_last' data_format)
+    by a factor of (height_factor, width_factor). Both factors should be
+    positive integers.
+    '''
+    if data_format == 'default':
+        data_format = K.image_data_format()
+    if data_format == 'channels_first':
+        original_shape = K.int_shape(X)
+        if target_height and target_width:
+            new_shape = tf.constant(np.array((target_height, target_width)).astype('int32'))
+        else:
+            new_shape = tf.shape(X)[2:]
+            new_shape *= tf.constant(np.array([height_factor, width_factor]).astype('int32'))
+        X = permute_dimensions(X, [0, 2, 3, 1])
+        X = tf.image.resize_bilinear(X, new_shape)
+        X = permute_dimensions(X, [0, 3, 1, 2])
+        if target_height and target_width:
+            X.set_shape((None, None, target_height, target_width))
+        else:
+            X.set_shape((None, None, original_shape[2] * height_factor, original_shape[3] * width_factor))
+        return X
+    elif data_format == 'channels_last':
+        original_shape = K.int_shape(X)
+        if target_height and target_width:
+            new_shape = tf.constant(np.array((target_height, target_width)).astype('int32'))
+        else:
+            new_shape = tf.shape(X)[1:3]
+            new_shape *= tf.constant(np.array([height_factor, width_factor]).astype('int32'))
+        X = tf.image.resize_bilinear(X, new_shape)
+        if target_height and target_width:
+            X.set_shape((None, target_height, target_width, None))
+        else:
+            X.set_shape((None, original_shape[1] * height_factor, original_shape[2] * width_factor, None))
+        return X
+    else:
+        raise Exception('Invalid data_format: ' + data_format)
 
 
 class BilinearUpSampling2D(Layer):
