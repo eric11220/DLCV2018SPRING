072884e50032aed87f940e71e85eff48ecdba657
eric11220 <b99902080@ntu.edu.tw>
2018-04-28 14:55:16 +0000

    get batch will return names now / n_classes from 21 to 7

--- hw3/src/data_loader.py
+++ hw3/src/data_loader.py
@@ -5,15 +5,14 @@ from scipy import misc
 
 class DataLoader():
     def __init__(self, data_dir, npz_path=None):
-        self._X, self._y = self._load_data(data_dir, npz_path)
-        self._y = self._raw_masks_to_cate(self._y)
+        self._X, self._y, self._names = self._load_data(data_dir, npz_path)
 
         self._start = 0
         self._num_data = self._X.shape[0]
 
         self._shuffle()
 
-    def _raw_masks_to_cate(self, masks, n_classes=21):
+    def _raw_masks_to_cate(self, masks, n_classes=7):
         masks = (masks >= 128).astype(int)
         masks = 4 * masks[:, :, :, 0] + 2 * masks[:, :, :, 1] + masks[:, :, :, 2]
         return masks
@@ -32,16 +31,16 @@ class DataLoader():
         # Load pre-saved images and masks
         if os.path.isfile(npz_path):
             info = np.load(npz_path)
-            X, y = info['arr_0'], info['arr_1']
+            X, y, img_names = info['arr_0'], info['arr_1'], info['arr_2']
         else:
-            X, y = [], []
+            X, y, img_names = [], [], []
             for cnt, img_name in enumerate(os.listdir(data_dir)):
-                if cnt % 100 == 0:
-                    print("Processed %d sat images and masks..." % cnt);
-    
                 name, ext = os.path.splitext(img_name)
                 if ext != '.jpg':
                     continue
+
+                if cnt % 100 == 0:
+                    print("Processed %d sat images and masks..." % cnt);
     
                 img_id, _ = name.split("_")
                 mask_name = "%s_mask.png" % img_id
@@ -51,16 +50,21 @@ class DataLoader():
     
                 X.append(misc.imread(sat_img_path))
                 y.append(misc.imread(mask_path))
+
+                idx, _ = name.split('_')
+                img_names.append(idx)
     
             X = np.asarray(X)
             y = np.asarray(y)
+            y = self._raw_masks_to_cate(y)
+            img_names = np.asarray(img_names)
     
-            np.savez(npz_path, X, y)
+            np.savez(npz_path, X, y, img_names)
     
         print("X shape: %s, y shape: %s" % (X.shape, y.shape))
-        return X, y
+        return X, y, img_names
 
-    def _cate_mask_to_one_hot(self, masks, n_classes=21):
+    def _cate_mask_to_one_hot(self, masks, n_classes=7):
         n, h, w = masks.shape
         one_hots = []
         for mask in masks:
@@ -73,14 +77,16 @@ class DataLoader():
         ind = np.random.permutation(self._num_data)
         self._X = self._X[ind]
         self._y = self._y[ind]
+        self._names = self._names[ind]
 
     def next_batch(self, batch_size=32):
         if self._start > self._num_data:
             self._shuffle()
-            return True, None, None
+            return True, None, None, None
         else:
             start, end = self._start, self._start + batch_size
             self._start += batch_size
             X = self._X[start:end]
             y = self._cate_mask_to_one_hot(self._y[start:end])
-            return False, X, y
+            names = self._names[start:end]
+            return False, X, y, names
