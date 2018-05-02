b1e68d047fbf2631fe2757bcb8c7cef878be70c3
eric11220 <b99902080@ntu.edu.tw>
2018-05-02 16:15:59 +0000

    Add wget to shell scripts

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 test.py $1 $2 32s.h5
+wget https://www.dropbox.com/s/ihm6rl98t2qsb3q/0.6436_epoch9.h5?dl=0
+python3 test.py $1 $2 0.6436_epoch9.h5?dl=0
--- hw3/hw3_best.sh
+++ hw3/hw3_best.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 test.py $1 $2 16s.h5
+wget https://www.dropbox.com/s/85wzh6igg85hler/0.6485.h5?dl=0
+python3 test.py $1 $2 0.6485.h5?dl=0
