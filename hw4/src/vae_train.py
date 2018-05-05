369e246f1383f3f5c26586bb46868b33b4b8bc94
eric11220 <b99902080@ntu.edu.tw>
2018-05-05 14:04:33 +0800

    Training script for VAE

---
+++ hw4/src/vae_train.py
@@ -0,0 +1,34 @@
+from data_loader import ImageLoader
+from models import *
+
+RECONST_DIR = "../reconst"
+
+def main():
+    n_epoch = 50
+
+    train_img_loader = ImageLoader("../data/train", "../data/train.npz")
+    train_imgs, paths = train_img_loader.getData()
+
+    test_img_loader = ImageLoader("../data/test", "../data/test.npz")
+    test_imgs, paths = test_img_loader.getData()
+    
+    #plt_reconst_img_callback = PlotReconstImages(test_imgs, RECONST_DIR)
+
+    vae = VAE((64, 64, 3))
+    vae.fit(train_imgs,
+           train_imgs,
+           epochs=n_epoch,
+           validation_data=(test_imgs, test_imgs))
+
+    '''
+    ae = autoencoder((64, 64, 3))
+    ae.fit(train_imgs,
+           train_imgs,
+           epochs=n_epoch,
+           validation_data=(test_imgs, test_imgs))
+           #callbacks=[plt_reconst_img_callback])
+    '''
+
+
+if __name__ == '__main__':
+    main()
