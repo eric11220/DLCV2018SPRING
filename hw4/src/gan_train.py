a088a3e18377bf21923f0c3c6cd13200e6226bab
eric11220 <b99902080@ntu.edu.tw>
2018-05-10 11:14:42 +0000

    n_epoch to 1e5 and save generated images per 500 steps

--- hw4/src/gan_train.py
+++ hw4/src/gan_train.py
@@ -4,7 +4,7 @@ from models import *
 RECONST_DIR = "../reconst"
 
 def main():
-    n_epoch = 50
+    n_epoch = 100000
 
     train_img_loader = ImageLoader("../data/train", "../data/train.npz")
     train_imgs, paths = train_img_loader.getData()
@@ -15,7 +15,7 @@ def main():
     #plt_reconst_img_callback = PlotReconstImages(test_imgs, RECONST_DIR)
 
     vae = GAN(train_imgs)
-    vae.train(n_epoch)
+    vae.train(n_epoch, sample_interval=500)
 
 
 if __name__ == '__main__':
