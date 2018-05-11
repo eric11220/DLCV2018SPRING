c21d02d0a423baba63771f028f80d59968503540
eric11220 <b99902080@ntu.edu.tw>
2018-05-11 02:43:40 +0000

    Load attributes as well

--- hw4/src/data_loader.py
+++ hw4/src/data_loader.py
@@ -7,13 +7,18 @@ from scipy import misc
 class ImageLoader():
     def __init__(self, data_dir, presaved_path):
         self._data_dir = data_dir
+
+        if data_dir[-1] == '/':
+            data_dir = data_dir[:-1]
+
         self._presaved_path = presaved_path
-        self._imgs, self._paths = self._load_data()
+        self._attr_path = "%s.csv" % data_dir
+
+        self._info = self._load_data()
 
     def _load_data(self):
         if os.path.isfile(self._presaved_path):
             info = np.load(self._presaved_path)
-            imgs, paths = info['imgs'], info['paths']
         else:
             paths, imgs = [], []
             for path in os.listdir(self._data_dir):
@@ -25,9 +30,25 @@ class ImageLoader():
 
             paths = np.asarray(paths)
             imgs = np.asarray(imgs)
-            np.savez(self._presaved_path, **{'imgs': imgs, 'paths': paths})
 
-        return imgs, paths
+            attr_dict = self._load_attrs(paths)
+            info = {**{'paths': paths, 'imgs': imgs}, **attr_dict}
+            np.savez(self._presaved_path, **info)
 
-    def getData(self):
-        return (self._imgs.astype(np.float32) - 127.5) / 127.5, self._paths
+        return info
+
+    def _load_attrs(self, paths):
+        attr_dict = {}
+        with open(self._attr_path, "r") as inf:
+            header = inf.readline().strip().split(',')[1:]
+            for line in inf:
+                info = line.strip().split(',')
+                path, attrs = info[0], info[1:]
+                attr_dict[path] = attrs
+
+        attrs = np.asarray([attr_dict[p] for p in paths], dtype=np.float32)
+        attr_dict = {name: attrs[:, idx] for idx, name in enumerate(header)}
+        return attr_dict
+
+    def getData(self, attr="Smiling"):
+        return (self._info['imgs'].astype(np.float32) - 127.5) / 127.5, self._info['paths'], self._info[attr]
