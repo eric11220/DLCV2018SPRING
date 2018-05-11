692431adbcfc385cae9c642174bf567d78a2c318
eric11220 <b99902080@ntu.edu.tw>
2018-05-11 02:45:11 +0000

    Change to single entry point for vae, dcgan and acgan

--- hw4/src/train.py
+++ hw4/src/train.py
@@ -1,21 +1,44 @@
+import argparse
 from data_loader import ImageLoader
 from models import *
 
 RECONST_DIR = "../reconst"
+def parse_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", help="Model to train", choices=["vae", "dcgan", "acgan"])
+    return parser.parse_args()
 
-def main():
-    n_epoch = 50
 
-    train_img_loader = ImageLoader("../data/train", "../data/train.npz")
-    train_imgs, paths = train_img_loader.getData()
-
-    test_img_loader = ImageLoader("../data/test", "../data/test.npz")
-    test_imgs, paths = test_img_loader.getData()
-    
+def train_vae(train_imgs, test_imgs, n_epoch=50):
     #plt_reconst_img_callback = PlotReconstImages(test_imgs, RECONST_DIR)
-
     vae = VAE(train_imgs, test_imgs)
     vae.train(n_epoch=n_epoch)
+
+
+def train_dcgan(imgs, n_epoch=100000):
+    dcgan = DCGAN(imgs)
+    dcgan.train(n_epoch, sample_interval=500)
+
+
+def train_acgan(imgs, attrs):
+    pass
+
+
+def main():
+    args = parse_input()
+
+    train_img_loader = ImageLoader("../data/train", "../data/train.npz")
+    train_imgs, _, train_attrs = train_img_loader.getData()
+
+    test_img_loader = ImageLoader("../data/test", "../data/test.npz")
+    test_imgs, _, test_attrs = test_img_loader.getData()
+
+    if args.model == "vae":
+        train_vae(train_imgs, test_imgs)
+    elif args.model == "dcgan":
+        train_dcgan(np.vstack((train_imgs, test_imgs)))
+    elif args.model == "acgan":
+        train_acgan(np.vstack((train_imgs, test_imgs)), np.concatenate((train_attrs, test_attrs)))
 
 
 if __name__ == '__main__':
