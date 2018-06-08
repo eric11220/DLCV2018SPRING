39f50f935cd2bae9d4627e6c564598459b6c12b3
eric11220 <b99902080@ntu.edu.tw>
2018-06-08 08:47:55 +0000

    Remove duplicate gt_valid.csv in the end

--- hw5/hw5_p2.sh
+++ hw5/hw5_p2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-python3 test.py --video_dir $1 --model_path models/trimmed_epoch8-val_acc:0.54-val_loss:1.83.hdf5 --output_path $3/p2_result.txt --part 2 --label_path $2/gt_valid.csv
+python3 test.py --video_dir $1 --model_path models/trimmed_epoch8-val_acc:0.54-val_loss:1.83.hdf5 --output_path $3/p2_result.txt --part 2 --label_path $2
