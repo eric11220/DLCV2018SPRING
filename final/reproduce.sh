1aa6f34a03d1017a4146098392a45649076aec73
eric11220 <b99902080@ntu.edu.tw>
2018-07-04 14:20:02 +0000

    Add README

--- final/reproduce.sh
+++ final/reproduce.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -x
 
 cd data
-python restructure.py --mode novel --n-valid 0 --output-dir ../src/Random-Erasing/ten_shot
+python3 restructure.py --mode novel --n-valid 0 --output-dir ../src/Random-Erasing/ten_shot
 
-cd src/Random-Erasing/
+cd ../src/Random-Erasing/
 python3 test.py checkpoint/model_best.pth.tar --mode test --out-file 1.csv  --kshot 1  --gather
 python3 test.py checkpoint/model_best.pth.tar --mode test --out-file 5.csv  --kshot 5  --gather
 python3 test.py checkpoint/model_best.pth.tar --mode test --out-file 10.csv --kshot 10 --gather
